d407dca93c048c3bea8e8bf083b18072ce5840fe
Aaron Huisinga <aaronhuisinga@gmail.com>
2020-06-26 15:05:52 -0500

    Fix description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
+      {hid: 'description', name: 'description', content: 'Software development with craft'},
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
@@ -43,7 +43,6 @@ export default {
   */
   buildModules: [
     '@nuxt/typescript-build',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
   ],
   /*
